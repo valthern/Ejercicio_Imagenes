97569bb9bb4a274f0862792ced4435d623b6c540
Omar I. Martínez Gómez <omar_238@hotmail.com>
2021-09-08 22:16:32 -0500

    adición de @ref no concluido

--- Ejercicio_Imagenes/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ Ejercicio_Imagenes/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,70 +13,70 @@ namespace Ejercicio_Imagenes.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Omar\source\ejemplos\Net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
+#line 1 "C:\Users\imartinez\source\ejemplos\net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Omar\source\ejemplos\Net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
+#line 2 "C:\Users\imartinez\source\ejemplos\net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Omar\source\ejemplos\Net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
+#line 3 "C:\Users\imartinez\source\ejemplos\net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Omar\source\ejemplos\Net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
+#line 4 "C:\Users\imartinez\source\ejemplos\net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Omar\source\ejemplos\Net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
+#line 5 "C:\Users\imartinez\source\ejemplos\net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Omar\source\ejemplos\Net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
+#line 6 "C:\Users\imartinez\source\ejemplos\net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Omar\source\ejemplos\Net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
+#line 7 "C:\Users\imartinez\source\ejemplos\net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Omar\source\ejemplos\Net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
+#line 8 "C:\Users\imartinez\source\ejemplos\net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Omar\source\ejemplos\Net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
+#line 9 "C:\Users\imartinez\source\ejemplos\net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
 using Ejercicio_Imagenes;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Omar\source\ejemplos\Net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
+#line 10 "C:\Users\imartinez\source\ejemplos\net5\Ejercicio_Imagenes\Ejercicio_Imagenes\_Imports.razor"
 using Ejercicio_Imagenes.Shared;
 
 #line default
@@ -90,6 +90,19 @@ using Ejercicio_Imagenes.Shared;
         {
         }
         #pragma warning restore 1998
+#nullable restore
+#line 10 "C:\Users\imartinez\source\ejemplos\net5\Ejercicio_Imagenes\Ejercicio_Imagenes\Pages\Index.razor"
+      
+    public string UrlImagen { get; set; }
+
+    public void OnChange()
+        {
+            UrlImagen= 
+        }
+
+#line default
+#line hidden
+#nullable disable
     }
 }
 #pragma warning restore 1591
